8def2ce64e939bc3f21ed35ea66f4a3c7892dcc6
Tariq Ahamad <tariqahamad@Tariqs-MacBook-Air.local>
2023-08-10 11:45:34 +0530

    navbar

--- components/header.tsx
+++ components/header.tsx
@@ -15,36 +15,34 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ session }) => {
   return (
-    <div>
+    <Container>
       logo
-    </div>
-    // <Container>
-    //   <div className="flex justify-between items-center">
-    //     <Link href="/">
-    //       <div className="relative h-[28px] w-[165px] md:h-[40px] md:w-[250px]">
-    //         <Image alt="Logo" src="/images/logo1.png" fill />
-    //       </div>
-    //     </Link>
+      {/* <div className="flex justify-between items-center">
+        <Link href="/">
+          <div className="relative h-[28px] w-[165px] md:h-[40px] md:w-[250px]">
+            <Image alt="Logo" src="/images/logo1.png" fill />
+          </div>
+        </Link>
 
-    //     <div className="flex items-center">
-    //       {session?.user ? (
-    //         <UserMenu user={session.user} />
-    //       ) : (
-    //         <Link
-    //           href="/sign-in"
-    //           className={
-    //             (buttonVariants({ variant: "ghost" }),
-    //             "p-2 rounded text-gray-800 hover:text-black hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-white")
-    //           }
-    //         >
-    //           <LogIn className="h-4 w-5" />
-    //         </Link>
-    //       )}
+        <div className="flex items-center">
+          {session?.user ? (
+            <UserMenu user={session.user} />
+          ) : (
+            <Link
+              href="/sign-in"
+              className={
+                (buttonVariants({ variant: "ghost" }),
+                "p-2 rounded text-gray-800 hover:text-black hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-white")
+              }
+            >
+              <LogIn className="h-4 w-5" />
+            </Link>
+          )}
 
-    //       <ToggleMenu />
-    //     </div>
-    //   </div>
-    // </Container>
+          <ToggleMenu />
+        </div>
+      </div> */}
+    </Container>
   );
 };
 export default Header;
